ac69222bd71b266f3b30c76d447856e5ae055356
Thanushree D N <thanushreedn2003@gmail.com>
2026-07-08 20:31:37 +0530

    Time: 7 ms (5.71%) | Memory: 45 MB (70.24%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,47 +1,82 @@
-class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+class Solution{
+    public int[] nextGreaterElement(int[] nums1, int[] nums2){
         int[] ans= new int[nums1.length];
-        for(int i=0; i< nums1.length; i++){
-            
-            Stack<Integer> st= new Stack<>();
+        int[] temp= new int[nums2.length];
+        Stack<Integer> st= new Stack<>();
 
-            for(int j=nums2.length-1; j>=0; j--){
-
+        for(int i=nums2.length-1; i>=0; i--){
+                while(!st.isEmpty() && st.peek()< nums2[i]){
+                    st.pop();
+                }
                 if(st.isEmpty()){
-
-                    if(nums2[j]== nums1[i]){
-                        ans[i]= -1;
-                        break;
-                    }
-                    st.push(nums2[j]);
+                    temp[i]= -1;
                 }else{
-                    if(st.peek() > nums2[j]){
-                        if(nums2[j]== nums1[i]){
-                            ans[i]= st.peek();
-                            break;
-                        }
-                        st.push(nums2[j]);
-                    }else{
-                        if(nums2[j]== nums1[i]){
-                            while(!st.isEmpty() && st.peek()<nums2[j]){
-                                st.pop();
-                            }if(st.isEmpty()){
-                                ans[i]= -1;
-                            }else{
-                                ans[i]= st.peek();
+                    temp[i]= st.peek();
+                }
+                st.push(nums2[i]);
+        }
 
-                            }
-                            break;
-                        }else{
-                            while(!st.isEmpty() && st.peek()<nums2[j]){
-                                st.pop();
-                            }
-                            st.push(nums2[j]);
-                        }
-                    }
+        for(int i=0; i<nums1.length; i++){
+            for(int j=0; j<nums2.length; j++){
+                if (nums2[j] ==nums1[i]){
+                    ans[i]= temp[j];
+                    break;
                 }
             }
         }
         return ans;
     }
 }
+
+
+
+
+
+//  O(n1 * n2)
+// class Solution {
+//     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+//         int[] ans= new int[nums1.length];
+//         for(int i=0; i< nums1.length; i++){
+            
+//             Stack<Integer> st= new Stack<>();
+
+//             for(int j=nums2.length-1; j>=0; j--){
+
+//                 if(st.isEmpty()){
+
+//                     if(nums2[j]== nums1[i]){
+//                         ans[i]= -1;
+//                         break;
+//                     }
+//                     st.push(nums2[j]);
+//                 }else{
+//                     if(st.peek() > nums2[j]){
+//                         if(nums2[j]== nums1[i]){
+//                             ans[i]= st.peek();
+//                             break;
+//                         }
+//                         st.push(nums2[j]);
+//                     }else{
+//                         if(nums2[j]== nums1[i]){
+//                             while(!st.isEmpty() && st.peek()<nums2[j]){
+//                                 st.pop();
+//                             }if(st.isEmpty()){
+//                                 ans[i]= -1;
+//                             }else{
+//                                 ans[i]= st.peek();
+
+//                             }
+//                             break;
+//                         }else{
+//                             while(!st.isEmpty() && st.peek()<nums2[j]){
+//                                 st.pop();
+//                             }
+//                             st.push(nums2[j]);
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
