3e9314e53dd32e616023c46011d8cf24c16fd7b0
Thanushree D N <thanushreedn2003@gmail.com>
2026-08-11 20:45:12 +0530

    Time: 1 ms (79.76%) | Memory: 43.6 MB (63.31%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -16,42 +16,86 @@
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
 
-        Queue<TreeNode> q= new LinkedList<>();
-        List<List<Integer>> ans= new ArrayList<>();
+        // Queue<TreeNode> q= new LinkedList<>();
+        // List<List<Integer>> ans= new ArrayList<>();
 
-        if(root == null) return ans;
+        // if(root == null) return ans;
 
-        q.add(root);
-        boolean flag= true;
+        // q.add(root);
+        // boolean flag= true;
 
-        while(!q.isEmpty()){
-            int size= q.size();
-            List<Integer> li= new ArrayList<>();
+        // while(!q.isEmpty()){
+        //     int size= q.size();
+        //     List<Integer> li= new ArrayList<>();
 
-            for(int i=0; i<size; i++){
-                TreeNode node= q.poll();
-                if(node.left != null){
-                    q.add(node.left);
-                }
-                if(node.right != null){
-                    q.add(node.right);
-                }      
-                li.add(node.val);
+        //     for(int i=0; i<size; i++){
+        //         TreeNode node= q.poll();
+        //         if(node.left != null){
+        //             q.add(node.left);
+        //         }
+        //         if(node.right != null){
+        //             q.add(node.right);
+        //         }      
+        //         li.add(node.val);
 
-            }
+        //     }
            
-            if(flag==true){
-                ans.add(li);
+        //     if(flag==true){
+        //         ans.add(li);
                
-            }
-            else{
-                Collections.reverse(li);
-                ans.add(li);
-            }
-            flag= !flag;
+        //     }
+        //     else{
+        //         Collections.reverse(li);
+        //         ans.add(li);
+        //     }
+        //     flag= !flag;
 
-        } 
-        return ans;
+        // } 
+        // return ans;
+        List<List<Integer>> result = new ArrayList<>();
+
+        // If the tree is empty, return an empty list
+        if (root == null) return result;
+
+        // Queue to store nodes for BFS (level order traversal)
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        // Boolean flag to control traversal direction
+        boolean leftToRight = true;
+
+        // Loop until all levels are processed
+        while (!q.isEmpty()) {
+            // Get the number of nodes at the current level
+            int size = q.size();
+
+            // Temporary array to store current level's values in zigzag order
+            Integer[] level = new Integer[size];
+
+            // Process each node in the current level
+            for (int i = 0; i < size; i++) {
+                // Get the front node from the queue
+                TreeNode node = q.poll();
+
+                // Determine index where this value should be stored
+                int index = leftToRight ? i : size - 1 - i;
+                level[index] = node.val;
+
+                // Add left child to queue if it exists
+                if (node.left != null) q.offer(node.left);
+                // Add right child to queue if it exists
+                if (node.right != null) q.offer(node.right);
+            }
+
+            // Flip traversal direction for the next level
+            leftToRight = !leftToRight;
+
+            // Add current level to the final result
+            result.add(Arrays.asList(level));
+        }
+
+        // Return the zigzag traversal result
+        return result;
         
 
         
